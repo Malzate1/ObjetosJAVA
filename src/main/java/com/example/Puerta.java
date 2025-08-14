52eabb34864d94667e1fac06d4b1246b880bcd63
Mayra Alzate <nadja.minni@gmail.com>
2025-08-14 17:59:02 -0500

    correciones en el main

--- src/main/java/com/example/Puerta.java
+++ src/main/java/com/example/Puerta.java
@@ -9,8 +9,6 @@ public class Puerta {
     private double peso;
     private String material;
 
-    
-
     public Puerta(String color, int numeroPartes, boolean cerradura, String tipoCerradura, double peso,
             String material) {
 
@@ -29,42 +27,41 @@ public class Puerta {
                 + ", tipoCerradura=" + tipoCerradura + ", peso=" + peso + ", material=" + material + "]";
     }
 
-
-
-    public boolean getCerradura(){
+    public boolean getCerradura() {
 
         return cerradura;
     }
 
-    public double getPeso(){
+    public double getPeso() {
 
         return peso;
     }
 
-    public void setColor(String color){
+    public void setColor(String color) {
 
-        this.color=color;
+        this.color = color;
 
     }
-    public void setTipoCerradura(){
+
+    public void setTipoCerradura() {
 
         this.tipoCerradura = tipoCerradura;
 
     }
 
-    public void abrir(){
-        if (cerradura==true) {
+    public void abrir() {
+        if (cerradura == true) {
             System.out.println("La puerta se puede abrir con llave.");
-        }else if(cerradura==false){
+        } else if (cerradura == false) {
             System.out.println("Debes buscar un cerrajero para que te ayude a abrir la puerta. ");
         }
     }
 
-    public void pintar(){
+    public void pintar() {
 
-        if ((color.equals("translucido")) || (color.equals("gris")) ){
+        if ((color.equals("translucido")) || (color.equals("gris"))) {
             System.out.println("La puerta esta pintada.");
-        }else {
+        } else {
             System.out.println("Hay que pintar la puerta. ");
         }
     }
